421652287d833f64eaf1741e1108cec96929bd63
cosmos <sensewan@gmail.com>
2021-03-31 15:21:50 +0900

    세번째 커밋-승

--- spring-scm01/src/main/java/com/springstudy/bbs/controller/BoardController.java
+++ spring-scm01/src/main/java/com/springstudy/bbs/controller/BoardController.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -46,6 +45,7 @@ public class BoardController {
 		return "boardList";
 	}
 	
+	//수정함
 	// 게시 글 상세보기 요청을 처리하는 메서드
 	@RequestMapping("/detail")
 	public String boardDetail(Model model, int no, 
